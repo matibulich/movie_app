22f712ed760b1be20b7c3c3f3c4fec8275b9d2a8
unknown <matias_bulich@hotmail.com>
2022-08-09 00:16:50 -0300

    arrelgo error

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('movie_app/', include('movie_app.urls')),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) #HEROKU
+]
 
 urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
