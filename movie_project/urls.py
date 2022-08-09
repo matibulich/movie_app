4852574fc9ed6030275f481faa9dbd4363d8a0fb
matibulich <92750218+matibulich@users.noreply.github.com>
2022-08-09 14:26:24 -0300

    Update urls.py

--- movie_project/urls.py
+++ movie_project/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('movie_app/', include('movie_app.urls')),
 ]
 
-urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
 urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT)
